9257951b952d5bfe400f4f2d4c13126eefc20690
timegated <beccdaniel@gmail.com>
2023-03-12 23:06:59 -0700

    writing api response to file

--- routes/jquery/index.ts
+++ routes/jquery/index.ts
@@ -1,7 +1,15 @@
 import * as express from "express";
 import { api } from '../../openai';
-
+import * as fs from 'fs';
 export const router = express.Router();
+
+// Prompt Templates;
+const classSelector = (promptText: string) => `that grabs text content from any class with name ${promptText}`;
+const idSelector = (promptText: string) => `that grabs text content form any id with name ${promptText}`;
+const attrSelector = (promptText: string) => `that grabs text content from any attribute with name ${promptText}`;
+const nestedSelector = (promptText: string) => `that grabs any content that is nested inside a span with data ${promptText}`;
+const elementSelector = (promptText: string) => `that grabs the text content of any html element with name ${promptText}`;
+const imgSelector = () => `that selects image urls and returns the data as a string`;
 
 router.get("/", async (req: express.Request, res: express.Response) => {
   try {
@@ -10,13 +18,8 @@ router.get("/", async (req: express.Request, res: express.Response) => {
     const prompt = req.query.prompt ? String(req.query.prompt) : '';
     const selectKey = req.query.selectKey ? String(req.query.selectKey) : 'class';
 
-    const classSelector = (promptText: string) => `that grabs text content from any class with name ${promptText}`;
-    const idSelector = (promptText: string) => `that grabs text content form any id with name ${promptText}`;
-    const attrSelector = (promptText: string) => `that grabs text content from any attribute with name ${promptText}`;
-    const nestedSelector = (promptText: string) => `that grabs any content that is nested inside a span with data ${promptText}`;
-    const imgSelector = () => `that selects image urls and returns the data as a string`;
 
-    const callMap = new Map([['id', idSelector], ['class', classSelector], ['attr', attrSelector], ['nested', nestedSelector], ['img', imgSelector]]);
+    const callMap = new Map([['id', idSelector], ['class', classSelector], ['attr', attrSelector], ['nested', nestedSelector], ['img', imgSelector], ['element', elementSelector]]);
 
     const callFunctionFor = (key: string) => {
       const funcCall = callMap.get(key);
@@ -27,7 +30,7 @@ router.get("/", async (req: express.Request, res: express.Response) => {
 
       return classSelector(prompt);
     };
-  
+
     const template = () => {
       return `You an experienced Javascript developer with experience using Jquery.
       Selector ${callFunctionFor(selectKey)}`
@@ -35,7 +38,7 @@ router.get("/", async (req: express.Request, res: express.Response) => {
 
     const maxTokens = Math.round(tokenLimit - template().length / 2.5);
 
-    const generateJqSelector = await api.createCompletion({
+    const jquery = await api.createCompletion({
       model: 'text-davinci-003',
       prompt: template(),
       n: 1,
@@ -44,9 +47,13 @@ router.get("/", async (req: express.Request, res: express.Response) => {
       suffix: 'using the JQuery Library without comments'
     });
 
-    const cleanText = generateJqSelector.data.choices[0].text?.replace(/\/\/(.+)/, '');
-
-    res.status(200).send(cleanText);
+    // Check for Valid jquery that can be copy/pasted/just works
+    const cleanText = (response: string | undefined) => {
+      const removeComment = /\/\/(.+)/;
+      return response?.replace(removeComment, '').replace('()', '');
+    }
+    fs.writeFileSync('./jquery.js', cleanText(jquery.data.choices[0].text) as string);
+    res.status(200).send(cleanText(jquery.data.choices[0].text));
   } catch (error: any) {
     if (error.response) {
       console.log(error.response.status);
