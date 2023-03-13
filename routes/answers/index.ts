967044bbfc5385e45650bd9c4beb24921ddb8aff
timegated <beccdaniel@gmail.com>
2023-03-12 22:36:26 -0700

    img and nestedSelector added for Jquery

--- routes/answers/index.ts
+++ routes/answers/index.ts
@@ -58,12 +58,13 @@ router.get("/completion", async (req: express.Request, res: express.Response) =>
     const prompt = req.query.prompt ? String(req.query.prompt) : '';
     const selectKey = req.query.selectKey ? String(req.query.selectKey) : 'class';
 
-    const classSelector = (promptText: string) => `that grabs textContent from any class with name ${promptText}`;
-    const idSelector = (promptText: string) => `that grabs textContent form any id with name ${promptText}`;
-    const attrSelector = (promptText: string) => `that grabs textContent from any attribute with name ${promptText}`;
+    const classSelector = (promptText: string) => `that grabs text content from any class with name ${promptText}`;
+    const idSelector = (promptText: string) => `that grabs text content form any id with name ${promptText}`;
+    const attrSelector = (promptText: string) => `that grabs text content from any attribute with name ${promptText}`;
+    const nestedSelector = (promptText: string) => `that grabs any content that is nested inside a span with data ${promptText}`
+    const imgSelector = () => `that selects image urls and returns the data as a string`;
 
-
-    const callMap = new Map([['id', idSelector], ['class', classSelector], ['attr', attrSelector]]);
+    const callMap = new Map([['id', idSelector], ['class', classSelector], ['attr', attrSelector], ['nested', nestedSelector], ['img', imgSelector]]);
 
     const callFunctionFor = (key: string) => {
       const funcCall = callMap.get(key);
@@ -91,7 +92,9 @@ router.get("/completion", async (req: express.Request, res: express.Response) =>
       suffix: 'using the JQuery Library without comments'
     });
 
-    res.status(200).send(generateJqSelector.data.choices[0].text?.replace(/\/\/(.+)/, ''));
+    const cleanText = generateJqSelector.data.choices[0].text?.replace(/\/\/(.+)/, '');
+
+    res.status(200).send(cleanText);
   } catch (error: any) {
     if (error.response) {
       console.log(error.response.status);
